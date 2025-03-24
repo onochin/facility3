2289a2529e77fa57fd16cda2c20a3dc36553276a
onochin <126230837+onochin@users.noreply.github.com>
2025-03-24 21:48:58 +0900

    Update routeLayer.js

--- routeLayer.js
+++ routeLayer.js
@@ -1,2 +1,16 @@
-// ここに前述のコードをコピーしてください。
-あああ
+export function addRouteLayer(map) {
+  map.addSource('route', {
+    type: 'geojson',
+    data: './data/route.geojson'
+  });
+
+  map.addLayer({
+    id: 'route-line',
+    type: 'line',
+    source: 'route',
+    paint: {
+      'line-color': '#007AFF',
+      'line-width': 4
+    }
+  });
+}
